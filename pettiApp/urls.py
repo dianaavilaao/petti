22b3ba7a40ec47ccd9bb3c8199cb28b584ed07c2
Paula Molina <pau.molina@duocuc.cl>
2024-10-14 21:59:36 -0300

    login html, view, settings, url

--- pettiApp/urls.py
+++ pettiApp/urls.py
@@ -1,10 +1,14 @@
 
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'pettiApp'
 
 urlpatterns = [
     path('', views.index, name='index'), 
     path('index/', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('admin/', views.administracion, name='administracion'),  
     path('busqueda-servicios/', views.busqueda_servicios, name='busqueda_servicios'),  
     path('inicio-sesion-admin/', views.inicio_sesion_admin, name='inicio_sesion_admin'),  
