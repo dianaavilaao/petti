4635013725d12812f7fe58622c630211504e0b9e
Paula Molina <pau.molina@duocuc.cl>
2024-11-04 21:53:28 -0300

    set email as username

--- pettiApp/admin.py
+++ pettiApp/admin.py
@@ -1,4 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django import forms
 from .models import *
 
-admin.site.register(User)
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username',)
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    list_display = ("email",)
+    ordering = ("date_joined",)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password', 'first_name', 'last_name')}),
+        )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password', 'first_name', 'last_name', 'is_superuser',
+                       'is_staff', 'is_active')}
+         ),
+    )
+
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
--- pettiApp/models.py
+++ pettiApp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from pettiApp.managers import UserManager
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+       
+    )
     is_administrator = models.BooleanField(default=False)
     is_partner = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
